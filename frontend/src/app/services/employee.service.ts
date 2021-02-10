910b32aaedfd5e3256ee64ac916cc2bd3e6b4b00
felipe-araya <felipe_araya_s@hotmail.com>
2021-02-11 00:10:18 +0100

    Frontend Connection with API

---
+++ frontend/src/app/services/employee.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Employee } from '../models/employee'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+  
+  URL_API = 'http://localhost:4000/api/employees'
+
+  employees: Employee[];
+
+  constructor(private http: HttpClient) { }
+
+  getEmployees(){
+
+    return this.http.get<Employee[]>(this.URL_API);
+    
+  }
+}
